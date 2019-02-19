e4aad1136fb18227f8d1a20f5ae3a5c16334d4d6
LINUX-PC <jesusrope98@outlook.com>
2019-02-19 22:32:11 +0100

    Updated url

--- lib/util/url.dart
+++ lib/util/url.dart
@@ -1,5 +1,5 @@
 class Url {
   //TODO add all URLs
   static const String sampleEdamam =
-      'https://api.edamam.com/search?q=chicken&app_id=541602a7&app_key=dc6e03b02796720e83b437f67e6074db%20&from=0&to=3&calories=591-722&health=alcohol-free';
+      'https://api.edamam.com/search?q=chicken&app_id=541602a7&app_key=dc6e03b02796720e83b437f67e6074db&to=20';
 }
