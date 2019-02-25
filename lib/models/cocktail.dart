6d700bd977d35faa0bcb317de661ac86115162a9
i62lucoc <i62lucoc@uco.es>
2019-02-25 23:01:22 +0100

    Se crean los modelos para recetas y cócteles

--- lib/models/cocktail.dart
+++ lib/models/cocktail.dart
@@ -6,4 +6,55 @@ class CocktailModel extends QueryModel {
     // TODO: implement loadData
     return null;
   }
+
+}
+
+class Cocktail {
+  final String name;
+  final String category;
+  final String glassType;
+  final String instruction;
+  final String photo;
+  final String ingr1;
+  final String ingr2;
+  final String ingr3;
+  final String ingr4;
+  final String msure1;
+  final String msure2;
+  final String msure3;
+  final String msure4;
+
+  Cocktail(
+    {this.name,
+    this.category,
+    this.glassType,
+    this.instruction,
+    this.photo,
+    this.ingr1,
+    this.ingr2,
+    this.ingr3,
+    this.ingr4,
+    this.msure1,
+    this.msure2,
+    this.msure3,
+    this.msure4
+    });
+
+  factory Cocktail.fromJson(Map<String, dynamic> json){
+    return Cocktail(
+        name: json["strDrink"],
+        category: json["strCategory"],
+        glassType: json["strGlass"],
+        instruction: json["strInstruction"],
+        photo: json["strDrinkThumb"],
+        ingr1: json["strIngredient1"],
+        ingr2: json["strIngredient2"],
+        ingr3: json["strIngredient3"],
+        ingr4: json["strIngredient4"],
+        msure1: json["strMeasure1"],
+        msure2: json["strMeasure2"],
+        msure3: json["strMeasure3"],
+        msure4: json["strMeasure4"]
+    );
+  }
 }
--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -1,10 +1,89 @@
 class Recipe {
-  // TODO add more parameters
   final String name;
+  final String photo;
+  final String recipeUrl;
+  final int yield;  //Raciones
+  final String diet;
+  final List health;
+  final List ingrLines;
+  final List ingr;
+  final double calories;
+  final double weight;
+  final double time;
+  final String energkcal;
+  final double quantenerg;
+  final String unitenerg;
+  final String fat;
+  final double quantfat;
+  final String unitfat;
+  final String sugar;
+  final double quantsugar;
+  final String unitsugar;
+  final String proteinkcal;
+  final double quantprotein;
+  final String unitprotein;
+  final String cholekcal;
+  final double quantchole;
+  final String unitchole;
 
-  Recipe({this.name});
+
+  Recipe({
+    this.name,
+    this.photo,
+    this.recipeUrl,
+    this.yield,  //Raciones
+    this.diet,
+    this.health,
+    this.ingrLines,
+    this.ingr,
+    this.calories,
+    this.weight,
+    this.time,
+    this.energkcal,
+    this.quantenerg,
+    this.unitenerg,
+    this.fat,
+    this.quantfat,
+    this.unitfat,
+    this.sugar,
+    this.quantsugar,
+    this.unitsugar,
+    this.proteinkcal,
+    this.quantprotein,
+    this.unitprotein,
+    this.cholekcal,
+    this.quantchole,
+    this.unitchole
+  });
 
   factory Recipe.fromJson(Map<String, dynamic> json) {
-    return Recipe(name: json['label']);
+    return Recipe(
+        name: json['label'],
+        photo: json['image'],
+        recipeUrl: json['url'],
+        yield: json['yield'],  //Raciones
+        diet: json['dietLabel'][0],
+        health: json['healthLabels'],
+        ingrLines: json['ingredientLines'],
+        ingr: json['ingredients'],
+        calories: json['calories'],
+        weight: json['totalWeight'],
+        time: json['totalTime'],
+        energkcal: json['totalNutrients']["ENERC_KCAL"]["label"],
+        quantenerg: json['totalNutrients']["ENERC_KCAL"]["quantity"],
+        unitenerg: json['totalNutrients']["ENERC_KCAL"]["unit"],
+        fat: json['totalNutrients']["FAT"]["label"],
+        quantfat: json['totalNutrients']["FAT"]["quantity"],
+        unitfat: json['totalNutrients']["FAT"]["unit"],
+        sugar: json['totalNutrients']["SUGAR"]["label"],
+        quantsugar: json['totalNutrients']["SUGAR"]["quantity"],
+        unitsugar: json['totalNutrients']["SUGAR"]["unit"],
+        proteinkcal: json['totalNutrients']["PROCNT"]["label"],
+        quantprotein: json['totalNutrients']["PROCNT"]["quantity"],
+        unitprotein: json['totalNutrients']["PROCNT"]["unit"],
+        cholekcal: json['totalNutrients']["CHOLE"]["label"],
+        quantchole: json['totalNutrients']["CHOLE"]["quantity"],
+        unitchole: json['totalNutrients']["CHOLE"]["unit"]
+    );
   }
 }
