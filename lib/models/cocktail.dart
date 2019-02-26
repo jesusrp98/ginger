1ff04f977d3a9e4fa05b1621a173e56c348c2500
i62lucoc <i62lucoc@uco.es>
2019-02-26 11:44:43 +0100

    Se arreglan los modelos de receta y cócteles

--- lib/models/cocktail.dart
+++ lib/models/cocktail.dart
@@ -6,42 +6,46 @@ class CocktailModel extends QueryModel {
     // TODO: implement loadData
     return null;
   }
-
 }
 
 class Cocktail {
-  final String name, category, glassType, instruction, photo;
-  final List ingredient, measure;
+  final String name, category, glassType, instruction, photo, tag;
+  final List ingredients, measures;
+  final bool isAlcoholic;
 
-  Cocktail(
-    {this.name,
+  Cocktail({
+    this.name,
     this.category,
+    this.tag,
     this.glassType,
     this.instruction,
     this.photo,
-    this.ingredient,
-    this.measure
-    });
+    this.ingredients,
+    this.measures,
+    this.isAlcoholic,
+  });
 
-  factory Cocktail.fromJson(Map<String, dynamic> json){
+  factory Cocktail.fromJson(Map<String, dynamic> json) {
     return Cocktail(
-        name: json["strDrink"],
-        category: json["strCategory"],
-        glassType: json["strGlass"],
-        instruction: json["strInstruction"],
-        photo: json["strDrinkThumb"],
-        ingredient: [
-          json["strIngredient1"],
-          json["strIngredient2"],
-          json["strIngredient3"],
-          json["strIngredient4"]
-        ],
-        measure: [
-          json["strMeasure1"],
-          json["strMeasure2"],
-          json["strMeasure3"],
-          json["strMeasure4"]
-        ]
+      name: json['strDrink'],
+      category: json['strCategory'],
+      tag: json['strIBA'],
+      isAlcoholic: true,
+      glassType: json['strGlass'],
+      instruction: json['strInstruction'],
+      photo: json['strDrinkThumb'],
+      ingredients: [
+        json['strIngredient1'],
+        json['strIngredient2'],
+        json['strIngredient3'],
+        json['strIngredient4'],
+      ],
+      measures: [
+        json['strMeasure1'],
+        json['strMeasure2'],
+        json['strMeasure3'],
+        json['strMeasure4'],
+      ],
     );
   }
 }
--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -1,38 +1,61 @@
 class Recipe {
-  final String name, photo, recipeUrl, diet;
-  final int servs; //Raciones
-  final List health, ingrLines, ingr;
+  final String name, photo, url, shareUrl;
+  final int servs;
+  final List healths, ingredients, diets;
   final double calories, weight, time;
-
+  final NutritionalValue sugar, fat, cholesterol, proteins;
 
   Recipe({
     this.name,
     this.photo,
-    this.recipeUrl,
-    this.servs, //Raciones
-    this.diet,
-    this.health,
-    this.ingrLines,
-    this.ingr,
+    this.url,
+    this.shareUrl,
+    this.servs,
+    this.diets,
+    this.healths,
+    this.ingredients,
     this.calories,
     this.weight,
     this.time,
+    this.sugar,
+    this.fat,
+    this.cholesterol,
+    this.proteins,
   });
 
   factory Recipe.fromJson(Map<String, dynamic> json) {
     return Recipe(
         name: json['label'],
         photo: json['image'],
-        recipeUrl: json['url'],
-        servs: json['yield'], //Raciones
-        diet: json['dietLabel'][0],
-        health: json['healthLabels'],
-        ingrLines: json['ingredientLines'],
-        ingr: json['ingredients'],
+        url: json['url'],
+        shareUrl: json['shareAs'],
+        servs: json['yield'],
+        diets: json['dietLabel'],
+        healths: json['healthLabels'],
+        ingredients: json['ingredientLines'],
         calories: json['calories'],
         weight: json['totalWeight'],
-        time: json['totalTime']
-    );
+        time: json['totalTime'],
+        sugar: NutritionalValue(
+          label: json['SUGAR']['label'],
+          quantity: json['SUGAR']['quantity'],
+          unit: json['SUGAR']['unit'],
+        ),
+        fat: NutritionalValue(
+          label: json['FAT']['label'],
+          quantity: json['FAT']['quantity'],
+          unit: json['FAT']['unit'],
+        ),
+        cholesterol: NutritionalValue(
+          label: json['CHOLE']['label'],
+          quantity: json['CHOLE']['quantity'],
+          unit: json['CHOLE']['unit'],
+        ),
+        proteins: NutritionalValue(
+          label: json['PROCNT']['label'],
+          quantity: json['PROCNT']['quantity'],
+          unit: json['PROCNT']['unit'],
+        ));
   }
 }
 
@@ -41,18 +64,13 @@ class NutritionalValue {
   final double quantity;
   final String unit;
 
-  NutritionalValue({
-    this.label,
-    this.quantity,
-    this.unit
-  });
+  NutritionalValue({this.label, this.quantity, this.unit});
 
   factory NutritionalValue.fromJson(Map<String, dynamic> json) {
     return NutritionalValue(
-        label: json['label'],
-        quantity: json['quantity'],
-        unit: json['unit']
+      label: json['label'],
+      quantity: json['quantity'],
+      unit: json['unit'],
     );
   }
-
 }
