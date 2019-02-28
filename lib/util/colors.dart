159d4be14734f9482022f4039cc3d5bed77e48e0
LINUX-PC <jesusrope98@outlook.com>
2019-02-28 17:06:51 +0100

    Added colors

--- lib/util/colors.dart
+++ lib/util/colors.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
 
+// App's colors
 const primaryColor = const Color(0xFFFFFFFF);
 const accentColor = const Color(0xFF66BB6A);
+
+/// RowItem's icon colors
+const acceptIcon = const Color(0xFF4CAF50);
+const denyIcon = const Color(0xFFF44336);
+const nullIcon = const Color(0xFF607D8B);
