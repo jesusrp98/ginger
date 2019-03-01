c3539317790d94e82a8fe87e12d999d82b9cd4e4
LINUX-PC <jesusrope98@outlook.com>
2019-03-01 22:58:20 +0100

    Improve search management

--- lib/models/search.dart
+++ lib/models/search.dart
@@ -6,7 +6,7 @@ import 'query.dart';
 import 'recipe.dart';
 
 class SearchModel extends QueryModel {
-  bool _success = false;
+  bool _success = true;
 
   @override
   Future loadData() {
@@ -14,18 +14,21 @@ class SearchModel extends QueryModel {
   }
 
   void fetchQuery(String query) async {
+    final String url = 'https://api.edamam.com/search?q=$query&app_id=541602a7&app_key=dc6e03b02796720e83b437f67e6074db&to=20';
+    List _result;
+
     setLoading(true);
-    response = await http.get('https://api.edamam.com/search?q=$query&app_id=541602a7&app_key=dc6e03b02796720e83b437f67e6074db&to=20');
+    response = await http.get(url);
     snapshot = json.decode(response.body);
 
-    // For demo purposes
-    print(snapshot);
-    print('https://api.edamam.com/search?q=$query&app_id=541602a7&app_key=dc6e03b02796720e83b437f67e6074db&to=20');
-
     clearItems();
-    items.addAll(snapshot['hits']
+    _result = snapshot['hits'];
+
+    items.addAll(_result
         .map((recipe) => Recipe.fromJson(recipe['recipe']))
         .toList());
+    
+    _success = _result.isNotEmpty;
 
     setLoading(false);
   }
--- lib/ui/tabs/search.dart
+++ lib/ui/tabs/search.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ginger/models/recipe.dart';
 import 'package:ginger/ui/pages/recipe.dart';
+import 'package:ginger/ui/tabs/big_tip.dart';
 import 'package:ginger/widgets/loading_indicator.dart';
 import 'package:scoped_model/scoped_model.dart';
 
@@ -17,7 +18,7 @@ class SearchTab extends StatelessWidget {
                 onSubmitted: (string) =>
                     string.isNotEmpty ? model.fetchQuery(string) : null,
                 decoration: InputDecoration(
-                  labelText: 'Search for recipes or cocktails',
+                  hintText: 'Search recipes or cocktails',
                   labelStyle: TextStyle(
                     color: Theme.of(context).textTheme.subhead.color,
                   ),
@@ -40,32 +41,53 @@ class SearchTab extends StatelessWidget {
                 ),
               ),
             ),
-            body: model.isLoading
-                ? LoadingIndicator()
-                : (model.items.isNotEmpty || model.onSuccess
-                    ? ListView.separated(
-                        itemCount: model.getItemCount,
-                        separatorBuilder: (context, index) => Divider(),
-                        itemBuilder: (context, index) {
-                          final Recipe recipe = model.getItem(index);
-                          return ListTile(
-                            leading: SizedBox(
-                              height: 64,
-                              width: 64,
-                              child: Image.network(recipe.photo),
-                            ),
-                            title: Text(recipe.name),
-                            subtitle: Text(recipe.servs.toString()),
-                            onTap: () => Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (_) => RecipePage(recipe),
-                                  ),
+            body: Builder(
+              builder: (context) {
+                if (model.isLoading) {
+                  return LoadingIndicator();
+                } else {
+                  if (model.items.isEmpty) {
+                    if (model.onSuccess)
+                      return Center(
+                        child: BigTip(
+                          icon: Icons.search,
+                          message: 'Start tying',
+                        ),
+                      );
+                    else
+                      return Center(
+                        child: BigTip(
+                          icon: Icons.sentiment_dissatisfied,
+                          message: 'We couldn\'t find anything',
+                        ),
+                      );
+                  } else {
+                    return ListView.separated(
+                      itemCount: model.getItemCount,
+                      separatorBuilder: (context, index) => Divider(),
+                      itemBuilder: (context, index) {
+                        final Recipe recipe = model.getItem(index);
+                        return ListTile(
+                          leading: SizedBox(
+                            height: 64,
+                            width: 64,
+                            child: Image.network(recipe.photo),
+                          ),
+                          title: Text(recipe.name),
+                          subtitle: Text(recipe.servs.toString()),
+                          onTap: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) => RecipePage(recipe),
                                 ),
-                          );
-                        },
-                      )
-                    : Center(child: Text('Just emptyness around here...'))),
+                              ),
+                        );
+                      },
+                    );
+                  }
+                }
+              },
+            ),
           ),
     );
   }
