1a1a54f7bb0f2e107d95b74bf9c9578dcf3bc12b
LINUX-PC <jesusrope98@outlook.com>
2019-03-08 20:20:19 +0100

    Updated library & widgets

--- lib/widgets/card_main_info.dart
+++ lib/widgets/card_main_info.dart
@@ -6,30 +6,32 @@ import 'separator.dart';
 class CardPhoto extends StatelessWidget {
   final String url;
   final String name;
+  final Widget body;
 
-  CardPhoto({
-    this.url,
-    this.name,
-  });
+  CardPhoto({this.url, this.name, this.body});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 6.0,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+      elevation: 6,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
       child: Padding(
-        padding: const EdgeInsets.all(24.0),
+        padding: const EdgeInsets.all(24),
         child: Row(
           children: <Widget>[
             HeroImage.card(url: url, tag: name),
-            Separator.spacer(width: 14, height: 0),
-            Text(
-              name,
-              style: Theme.of(context)
-                  .textTheme
-                  .title
-                  .copyWith(fontWeight: FontWeight.bold),
-            ),
+            Separator.spacer(width: 14),
+            Expanded(
+              child: Text(
+                name,
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+                style: Theme.of(context)
+                    .textTheme
+                    .title
+                    .copyWith(fontWeight: FontWeight.bold),
+              ),
+            )
           ],
         ),
       ),
